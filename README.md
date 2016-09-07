da9b0bfb79506e3be435d2ff6e1233b337dc94b7
Rolf Sievers <rolf@Butterfly.home>
2016-09-07 23:12:01 +0200

    Start playing with the Elm architecture.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Moni Worksheet
+
+Another go at building the Exercise management and Worksheet creation tool
+I envisioned for quite a while.
---
+++ src/main.elm
@@ -0,0 +1,102 @@
+import Html exposing (..)
+import Html.Attributes exposing (class, type', placeholder, value)
+import Html.App as Html
+import Html.Events exposing (..)
+import Random
+
+
+
+main =
+  Html.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+
+-- MODEL
+
+
+type alias Model =
+  { exercises : List Exercise
+  , newExercise : Exercise
+  }
+
+type alias Exercise =
+  { title : String
+  , text  : String
+  }
+
+blankExercise = (Exercise "" "")
+
+
+init : (Model, Cmd Msg)
+init =
+  (Model [Exercise "Hello" "World"] blankExercise, Cmd.none)
+
+
+
+-- UPDATE
+
+
+type Msg
+  = UpdateTitle String
+  | UpdateText String
+  | AddExcercise
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    UpdateTitle title ->
+      let
+        oldExercise = model.newExercise
+      in
+        ({ model | newExercise = { oldExercise | title = title }}, Cmd.none)
+
+    UpdateText text ->
+      let
+        oldExercise = model.newExercise
+      in
+        ({ model | newExercise = { oldExercise | text = text }}, Cmd.none)
+
+    AddExcercise ->
+      ({ model | newExercise = blankExercise, exercises = model.newExercise :: model.exercises}, Cmd.none)
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ h1 [] [ text "Caption" ]
+    , div []
+      [ input [ type' "text", placeholder "Title", onInput UpdateTitle, value model.newExercise.title ] []
+      , input [ type' "text", placeholder "Text", onInput UpdateText, value model.newExercise.text ] []
+      , button [ onClick AddExcercise ] [ text "Add to List" ]
+      ]
+    , li [] <| List.map renderExercise model.exercises
+    ]
+
+
+renderExercise : Exercise -> Html Msg
+renderExercise exercise =
+  div [ class "summary" ]
+    [ div []
+      [ h1 [] [ text (exercise.title) ]
+      , span [ class "summary-hints" ] []
+      , p [ class "summary-text" ] [ text exercise.text ]
+      ]
+    ]
